a49028441dc42e42c98869c2f03be8e56c2bf37d
Gunar Fatyhov <gunarf@yandex.ru>
2024-11-03 14:34:07 +0300

    HM1

--- Lesson_3/Lesson_3.java
+++ Lesson_3/Lesson_3.java
@@ -1,5 +1,14 @@
 package Lesson_3;
 
 public class Lesson_3 {
-
+    
+    public static void main(String[] args){
+        printThreeWords();
+    }
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
 }
+
