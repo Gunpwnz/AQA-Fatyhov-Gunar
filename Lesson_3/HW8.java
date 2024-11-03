64fe172f8fa6c2076be32cd8aa36c46df32e33db
Gunar Fatyhov <gunarf@yandex.ru>
2024-11-03 17:39:47 +0300

    Create HW8.java

---
+++ Lesson_3/HW8.java
@@ -0,0 +1,19 @@
+package Lesson_3;
+
+
+public class HW8 {
+    public static void main(String[] args) {
+        String text = "Privet";
+        int times = 5;
+        numbers(text, times);
+        
+    }
+
+    public static void numbers(String txt, int chislo) {
+        for (int i = 0; i < chislo; i++){
+            System.out.println(txt);
+        }
+        
+        
+    }
+}
