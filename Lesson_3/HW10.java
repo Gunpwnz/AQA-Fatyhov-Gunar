6a58b1e73354e306bc70e92c1d5688a2e53d389e
Gunar Fatyhov <gunarf@yandex.ru>
2024-11-03 19:49:14 +0300

    Create HW10.java

---
+++ Lesson_3/HW10.java
@@ -0,0 +1,17 @@
+package Lesson_3;
+
+public class HW10 {
+    public static void main(String[] args) {
+        int[] numb = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < numb.length; i++) {
+            if (numb[i] == 0) {
+                System.out.println(numb[i] = 1);
+            }
+            else {
+                System.out.println(numb[i] = 0);
+            }
+                
+        }
+    }
+
+}
