108893248147367f81c53f12057782dc68f327b0
Gunar Fatyhov <gunarf@yandex.ru>
2024-11-03 13:04:19 +0300

    Create FirstApp.java

---
+++ FirstApp.java/FirstApp.java
@@ -0,0 +1,11 @@
+package FirstApp.java;
+
+public class FirstApp {
+    public FirstApp() {
+    }
+ 
+    public static void main(String[] var0) {
+       System.out.println("Hello, World!");
+    }
+
+}
