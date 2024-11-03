6869db70b347bd493e55628df3dfbf1b550d6894
Gunar Fatyhov <gunarf@yandex.ru>
2024-11-03 16:05:28 +0300

    Create HW4.java

---
+++ Lesson_3/HW4.java
@@ -0,0 +1,19 @@
+package Lesson_3;
+
+public class HW4 {
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+
+    public static void compareNumbers() {
+        int a = 10;
+        int b = 10;
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        
+        else {
+            System.out.println("a < b");
+        }
+    }
+}
