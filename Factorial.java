f51c06b60fdc350fe4457bf3fa40c5c3ada966ce
Gunar Fatyhov <gunarf@yandex.ru>
2024-12-14 16:42:53 +0300

    add factorial

--- Factorial.java
+++ Factorial.java
@@ -1,3 +1,12 @@
 public class Factorial {
-
+    public static long calculate(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Number must be non-negative");
+        }
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
 }
